d5f37c63c997b50565990c8578de3434072e6677
MashaTrunina <147832180+MashaTrunina@users.noreply.github.com>
2024-05-15 15:18:21 +0300

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -3,24 +3,21 @@
 #include <thread>
 #include <cstring>
 
-constexpr size_t PAGE_SIZE = 4 * 1024; // Размер страницы (4 КБ)
+constexpr size_t PAGE_SIZE = 4 * 1024; // Размер страницы 
 
 // Функция для доступа к памяти с указанным шагом и заполнением
 void memoryAccess(size_t totalMemoryBytes, unsigned int delayPages, char fillValue) {
     for (size_t offset = 0; offset < totalMemoryBytes; offset += PAGE_SIZE) {
-        char* memory = new char[PAGE_SIZE]; // Выделение памяти размером PAGE_SIZE
+        char* memory = new char[PAGE_SIZE]; 
 
-        // Заполнение выделенной памяти указанным значением
+     
         std::memset(memory, fillValue, PAGE_SIZE);
 
         // Задержка через определенное количество страниц
         if (offset % (delayPages * PAGE_SIZE) == 0) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Задержка 100 миллисекунд
+            std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
         }
 
-        // Здесь может быть код для записи данных в память
-        // Но в данном случае мы просто наполнили память указанным символом
-        // Поскольку в этой версии кода требуется только запись, ничего дополнительно делать не нужно
     }
 }
 
